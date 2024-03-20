60deb2ed09df77a920946329e0e6c6de21016c6f
rocimarquez <127412171+rocimarquez@users.noreply.github.com>
2024-03-20 16:24:41 -0300

    Delete app directory

--- app/models/match.rb
+++
@@ -1,4 +0,0 @@
-class Match < ApplicationRecord
-    belongs_to :teamA, class_name: "Team"
-    belongs_to :teamB, class_name: "Team"
-end
--- app/models/team.rb
+++
@@ -1,5 +0,0 @@
-class Team < ApplicationRecord
-    has_many :players, dependent: :destroy
-    has_many :matches, dependent: :destroy
-    
-end 
